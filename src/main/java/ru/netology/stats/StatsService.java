bcf118c815bd3bfe67fba322b48f36f05f476d9f
Marina.Sachko <marina.sachko@mail.ru>
2023-05-11 21:33:07 +0300

    Reformat code

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -1,4 +1,5 @@
 package ru.netology.stats;
+
 public class StatsService {
 
     public int getSumSales(int[] arrSales) {
@@ -17,32 +18,29 @@ public class StatsService {
 
     public int getMaxSalesMonth(int[] arrSales) {
         int maxSalesMonth = 0;
-        for (int i=0; i<arrSales.length; i++) {
-            if (arrSales[maxSalesMonth] <= arrSales[i])
-
-        {
+        for (int i = 0; i < arrSales.length; i++) {
+            if (arrSales[maxSalesMonth] <= arrSales[i]) {
                 maxSalesMonth = i;
             }
         }
-        return maxSalesMonth+1;
+        return maxSalesMonth + 1;
     }
+
     public int getMinSalesMonth(int[] arrSales) {
         int minSalesMonth = 0;
-        for (int i=0; i<arrSales.length; i++) {
-            if (arrSales[minSalesMonth] >= arrSales[i])
-
-            {
+        for (int i = 0; i < arrSales.length; i++) {
+            if (arrSales[minSalesMonth] >= arrSales[i]) {
                 minSalesMonth = i;
             }
         }
-        return minSalesMonth+1;
+        return minSalesMonth + 1;
     }
+
     public int getCountMonthMaxSales(int[] arrSales) {
         int countMonthmax = 0;
-        int average = getAverageAmountSalesMonth(arrSales) ;
-        for (int i=0; i<arrSales.length; i++) {
-            if (arrSales[i] > average)
-            {
+        int average = getAverageAmountSalesMonth(arrSales);
+        for (int i = 0; i < arrSales.length; i++) {
+            if (arrSales[i] > average) {
                 countMonthmax = countMonthmax + 1;
             }
         }
@@ -51,10 +49,9 @@ public class StatsService {
 
     public int getCountMonthMinSales(int[] arrSales) {
         int countMonthmin = 0;
-        int average = getAverageAmountSalesMonth(arrSales) ;
-        for (int i=0; i<arrSales.length; i++) {
-            if (arrSales[i] < average)
-            {
+        int average = getAverageAmountSalesMonth(arrSales);
+        for (int i = 0; i < arrSales.length; i++) {
+            if (arrSales[i] < average) {
                 countMonthmin = countMonthmin + 1;
             }
         }
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -8,26 +8,29 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class StatsServiceTest {
 
-    int[] arrSales = {7, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18 };
+    int[] arrSales = {7, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
     @Test
     public void shouldFindSumSales() {
         StatsService service = new StatsService();
 
-        System.out.println("Сумма всех продаж "+service.getSumSales(arrSales));
+        System.out.println("Сумма всех продаж " + service.getSumSales(arrSales));
 
     }
+
     @Test
     public void shouldFindAveradgeAmountSale() {
         StatsService service = new StatsService();
 
-        System.out.println("Средняя сумма продаж в месяц "+service.getAverageAmountSalesMonth(arrSales));
+        System.out.println("Средняя сумма продаж в месяц " + service.getAverageAmountSalesMonth(arrSales));
 
-     }
+    }
+
     @Test
     public void shouldFindMaxSalesMonth() {
         StatsService service = new StatsService();
 
-        System.out.println("Номер месяца, в котором был пик продаж "+service.getMaxSalesMonth(arrSales));
+        System.out.println("Номер месяца, в котором был пик продаж " + service.getMaxSalesMonth(arrSales));
 
     }
 
@@ -35,14 +38,15 @@ public class StatsServiceTest {
     public void shouldFindMinSalesMonth() {
         StatsService service = new StatsService();
 
-        System.out.println("Номер месяца, в котором был минимум продаж "+service.getMinSalesMonth(arrSales));
+        System.out.println("Номер месяца, в котором был минимум продаж " + service.getMinSalesMonth(arrSales));
 
     }
+
     @Test
     public void shouldFindCountMonthMinSales() {
         StatsService service = new StatsService();
 
-        System.out.println("Количество месяцев, в которых продажи были ниже среднего "+service.getCountMonthMinSales(arrSales));
+        System.out.println("Количество месяцев, в которых продажи были ниже среднего " + service.getCountMonthMinSales(arrSales));
 
     }
 
@@ -50,7 +54,7 @@ public class StatsServiceTest {
     public void shouldFindCountMonthMaxSales() {
         StatsService service = new StatsService();
 
-        System.out.println("количество месяцев, в которых продажи были выше среднего "+service.getCountMonthMaxSales(arrSales));
+        System.out.println("количество месяцев, в которых продажи были выше среднего " + service.getCountMonthMaxSales(arrSales));
 
     }
 
